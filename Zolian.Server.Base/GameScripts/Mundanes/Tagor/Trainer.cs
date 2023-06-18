b217fc2b18cc3397ac704ebef403ada8cb0128e4
FallenDev <Paul.Buck@TheBuckNetwork.com>
2023-06-18 02:11:48 -0400

    Tagor Training Academy Fix

--- Zolian.Server.Base/GameScripts/Mundanes/Tagor/Trainer.cs
+++ Zolian.Server.Base/GameScripts/Mundanes/Tagor/Trainer.cs
@@ -85,146 +85,139 @@ public class Trainer : MundaneScript
                 client.CloseDialog();
                 break;
             case 0x0001:
-            {
-                var options = new List<OptionsDataItem>();
-
-                if (_skillList.Count > 0)
                 {
-                    options.Add(new(0x03, "Let's get started"));
-                    options.Add(new OptionsDataItem(0x00, "{=bNo, thank you"));
-                    client.SendOptionsDialog(Mundane, "Ready? It'll cost you 100,000 gold for unlimited time.", options.ToArray());
-                }
-                else
-                {
-                    client.CloseDialog();
-                    client.SendMessage(0x02, "Ah, come back anytime.");
-                }
+                    var options = new List<OptionsDataItem>();
 
-                break;
-            }
-            case 0x0002:
-            {
-                client.SendSkillForgetDialog(Mundane,
-                    "Muscle memory is a hard thing to unlearn. \nYou may come back to relearn what the mind has lost but the muscle still remembers.", 0x9000);
-                break;
-            }
-            case 0x9000:
-            {
-                int.TryParse(args, out var idx);
-
-                if (idx is < 0 or > byte.MaxValue)
-                {
-                    client.SendMessage(0x02, "You don't quite have that skill.");
-                    client.CloseDialog();
-                }
-
-                client.Aisling.SkillBook.Remove((byte)idx, true);
-                client.Send(new ServerFormat2D((byte)idx));
-                client.LoadSkillBook();
-
-                client.SendSkillForgetDialog(Mundane, "Your body is still, breathing in, relaxed. \nAny other skills you wish to forget?", 0x9000);
-                break;
-            }
-            case 0x0003:
-            {
-                if (client.Aisling.GoldPoints >= 100000)
-                {
-                    client.Aisling.GoldPoints -= 100000;
-                    client.TransitionToMap(5257, new Position(17, 17));
-                    await Task.Delay(100).ContinueWith(ct =>
+                    if (_skillList.Count > 0)
+                    {
+                        options.Add(new(0x03, "Let's get started"));
+                        options.Add(new OptionsDataItem(0x00, "{=bNo, thank you"));
+                        client.SendOptionsDialog(Mundane, "Ready? It'll cost you 100,000 gold for unlimited time.", options.ToArray());
+                    }
+                    else
                     {
                         client.CloseDialog();
-                        var spot = _dojoSpots.RandomIEnum();
-                        client.Aisling.Pos = spot;
-                        client.ClientRefreshed();
-                        var monsters = client.Aisling.MonstersNearby()
-                            .Where(i => i.WithinRangeOf(client.Aisling, 2));
-
-                        foreach (var monster in monsters)
-                        {
-                            client.Aisling.Facing(monster.X, monster.Y, out var direction);
-                            if (!client.Aisling.Position.IsNextTo(monster.Position)) return;
-                            client.Aisling.Direction = (byte)direction;
-                            client.Aisling.Turn();
-                        }
-
-
-                    });
-
-                    var monsters = client.Aisling.MonstersNearby()
-                        .Where(i => i.WithinRangeOf(client.Aisling, 2));
-
-                    foreach (var monster in monsters)
-                    {
-                        client.Aisling.Target = monster;
-                        break;
+                        client.SendMessage(0x02, "Ah, come back anytime.");
                     }
 
-                    while (client.Aisling.NextTo(client.Aisling.Target.X, client.Aisling.Target.Y))
+                    break;
+                }
+            case 0x0002:
+                {
+                    client.SendSkillForgetDialog(Mundane,
+                        "Muscle memory is a hard thing to unlearn. \nYou may come back to relearn what the mind has lost but the muscle still remembers.", 0x9000);
+                    break;
+                }
+            case 0x9000:
+                {
+                    int.TryParse(args, out var idx);
+
+                    if (idx is < 0 or > byte.MaxValue)
                     {
-                        await Task.Delay(500).ContinueWith(ct =>
+                        client.SendMessage(0x02, "You don't quite have that skill.");
+                        client.CloseDialog();
+                    }
+
+                    client.Aisling.SkillBook.Remove((byte)idx, true);
+                    client.Send(new ServerFormat2D((byte)idx));
+                    client.LoadSkillBook();
+
+                    client.SendSkillForgetDialog(Mundane, "Your body is still, breathing in, relaxed. \nAny other skills you wish to forget?", 0x9000);
+                    break;
+                }
+            case 0x0003:
+                {
+                    if (client.Aisling.GoldPoints >= 100000)
+                    {
+                        client.Aisling.GoldPoints -= 100000;
+                        client.TransitionToMap(5257, new Position(17, 17));
+                        await Task.Delay(100).ContinueWith(ct =>
                         {
-                            foreach (var skill in _skillList)
+                            client.CloseDialog();
+                            var spot = _dojoSpots.RandomIEnum();
+                            client.Aisling.Pos = spot;
+                            client.ClientRefreshed();
+                            var monsters = client.Aisling.MonstersNearby()
+                                .Where(i => i.WithinRangeOf(client.Aisling, 2));
+
+                            foreach (var monster in monsters)
                             {
-                                if (skill is null) continue;
-                                if (skill.Template.SkillType == SkillScope.Assail) continue;
-                                if (!skill.CanUse()) continue;
-                                if (skill.Scripts is null || skill.Scripts.Count == 0) continue;
-
-                                skill.InUse = true;
-
-                                var script = skill.Scripts.Values.First();
-                                script?.OnUse(client.Aisling);
-
-                                skill.InUse = false;
-                                skill.CurrentCooldown = skill.Template.Cooldown;
+                                client.Aisling.Facing(monster.X, monster.Y, out var direction);
+                                if (!client.Aisling.Position.IsNextTo(monster.Position)) return;
+                                client.Aisling.Direction = (byte)direction;
+                                client.Aisling.Turn();
                             }
 
-                            GameServer.Assail(client);
-                        });
-                    }
-                }
-                else
-                {
-                    client.SendOptionsDialog(Mundane, "Looks like you don't have enough, come back when you do. (100,000 gold)");
-                }
-                break;
-            }
-            case 0x0004:
-            {
-                // add logic to warp to a training dummy, then start improving the skill they've picked
 
-                break;
-            }
+                        });
+
+                        var monster = client.Aisling.MonstersNearby().FirstOrDefault(i => i.WithinRangeOf(client.Aisling, 2));
+                        client.Aisling.Target = monster;
+
+                        while (client.Aisling.NextTo(client.Aisling.Target!.X, client.Aisling.Target!.Y))
+                        {
+                            await Task.Delay(500).ContinueWith(ct =>
+                            {
+                                foreach (var skill in client.Aisling.SkillBook.Skills.Values)
+                                {
+                                    if (skill is null) continue;
+                                    if (!skill.CanUse()) continue;
+                                    if (skill.Scripts is null || skill.Scripts.IsEmpty) continue;
+
+                                    skill.InUse = true;
+
+                                    var script = skill.Scripts.Values.First();
+                                    script?.OnUse(client.Aisling);
+
+                                    skill.InUse = false;
+                                    skill.CurrentCooldown = skill.Template.Cooldown;
+                                }
+
+                                GameServer.Assail(client);
+                            });
+                        }
+                    }
+                    else
+                    {
+                        client.SendOptionsDialog(Mundane, "Looks like you don't have enough, come back when you do. (100,000 gold)");
+                    }
+                    break;
+                }
+            case 0x0004:
+                {
+                    // add logic to warp to a training dummy, then start improving the skill they've picked
+
+                    break;
+                }
 
             #endregion
 
             #region Spells
 
             case 0x0011:
-            {
-                client.SendSpellForgetDialog(Mundane, "The mind is a complex place, sometimes we need to declutter. \nBe warned, This cannot be undone.", 0x0800);
-                break;
-            }
-            case 0x0800:
-            {
-                int.TryParse(args, out var idx);
-
-                if (idx is < 0 or > byte.MaxValue)
                 {
-                    client.SendMessage(0x02, "I do not sense this spell within you any longer.");
-                    client.CloseDialog();
+                    client.SendSpellForgetDialog(Mundane, "The mind is a complex place, sometimes we need to declutter. \nBe warned, This cannot be undone.", 0x0800);
+                    break;
+                }
+            case 0x0800:
+                {
+                    int.TryParse(args, out var idx);
+
+                    if (idx is < 0 or > byte.MaxValue)
+                    {
+                        client.SendMessage(0x02, "I do not sense this spell within you any longer.");
+                        client.CloseDialog();
+                    }
+
+                    client.Aisling.SpellBook.Remove((byte)idx, true);
+                    client.Send(new ServerFormat18((byte)idx));
+                    client.LoadSpellBook();
+
+                    client.SendSpellForgetDialog(Mundane, "It is gone, Shall we cleanse more?\nRemember, This cannot be undone.", 0x0800);
+                    break;
                 }
 
-                client.Aisling.SpellBook.Remove((byte)idx, true);
-                client.Send(new ServerFormat18((byte)idx));
-                client.LoadSpellBook();
-
-                client.SendSpellForgetDialog(Mundane, "It is gone, Shall we cleanse more?\nRemember, This cannot be undone.", 0x0800);
-                break;
-            }
-
-            #endregion
+                #endregion
         }
     }
 
