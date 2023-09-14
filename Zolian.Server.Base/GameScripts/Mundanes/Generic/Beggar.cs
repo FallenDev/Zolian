ba9e429efa3e50295637bf485eceb648fd9f0713
FallenDev <Paul.Buck@TheBuckNetwork.com>
2023-09-13 23:26:17 -0400

    Beggar Npcs

---
+++ Zolian.Server.Base/GameScripts/Mundanes/Generic/Beggar.cs
@@ -0,0 +1,37 @@
+﻿using Chaos.Common.Definitions;
+using Darkages.Enums;
+using Darkages.Network.Client;
+using Darkages.Network.Server;
+using Darkages.ScriptingBase;
+using Darkages.Sprites;
+
+namespace Darkages.GameScripts.Mundanes.Generic;
+
+[Script("Beggar")]
+public class Beggar : MundaneScript
+{
+    public Beggar(WorldServer server, Mundane mundane) : base(server, mundane) { }
+    public override void OnClick(WorldClient client, uint serial) { }
+    public override void OnResponse(WorldClient client, ushort responseID, string args) { }
+
+    public override void OnItemDropped(WorldClient client, Item item)
+    {
+        client.Aisling.SendTargetedClientMethod(Scope.NearbyAislings, c => c.SendPublicMessage(Mundane.Serial, PublicMessageType.Normal, "Beggar: I'll make good use of it, that I will"));
+        client.Aisling.Inventory.RemoveFromInventory(client, item);
+    }
+
+    public override void OnGoldDropped(WorldClient client, uint gold)
+    {
+        if (client.Aisling.GoldPoints >= gold)
+        {
+            client.Aisling.GoldPoints -= gold;
+            client.GiveExp((int)gold, true);
+            client.SendAttributes(StatUpdateType.ExpGold);
+            client.SendServerMessage(ServerMessageType.ActiveMessage, $"Gained {gold} experience!");
+            client.Aisling.SendTargetedClientMethod(Scope.NearbyAislings, c => c.SendPublicMessage(Mundane.Serial, PublicMessageType.Normal, "Beggar: Blessed the stars!!!"));
+            return;
+        }
+
+        client.Aisling.SendTargetedClientMethod(Scope.NearbyAislings, c => c.SendPublicMessage(Mundane.Serial, PublicMessageType.Normal, "Beggar: Ya makin a foll of meh?"));
+    }
+}
