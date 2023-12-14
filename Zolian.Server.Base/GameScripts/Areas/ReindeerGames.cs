ae025fdac503c7b15d884290d3912a844386af69
FallenDev <paul.buck@TheBuckNetwork.com>
2023-12-14 12:14:56 -0500

    Reindeer Games phased zone

---
+++ Zolian.Server.Base/GameScripts/Areas/ReindeerGames.cs
@@ -0,0 +1,35 @@
+﻿using Darkages.Network.Client;
+using Darkages.ScriptingBase;
+using Darkages.Sprites;
+using Darkages.Types;
+
+using System.Collections.Concurrent;
+using System.Numerics;
+
+namespace Darkages.GameScripts.Areas;
+
+[Script("Reindeer Games")]
+public class ReindeerGames : AreaScript
+{
+    private readonly ConcurrentDictionary<long, Aisling> _playersOnMap = new();
+    public ReindeerGames(Area area) : base(area) => Area = area;
+    public override void Update(TimeSpan elapsedTime) { }
+    public override void OnMapEnter(WorldClient client) => _playersOnMap.TryAdd(client.Aisling.Serial, client.Aisling);
+    public override void OnMapExit(WorldClient client) => _playersOnMap.TryRemove(client.Aisling.Serial, out _);
+
+    public override void OnPlayerWalk(WorldClient client, Position oldLocation, Position newLocation)
+    {
+        var vectorMap = new Vector2(newLocation.X, newLocation.Y);
+        if (client.Aisling.Pos != vectorMap) return;
+        _playersOnMap.TryAdd(client.Aisling.Serial, client.Aisling);
+
+        switch (newLocation.X)
+        {
+            case 12 when newLocation.Y == 0:
+                client.TransitionToMap(client.Aisling.QuestManager.RescuedReindeer ? 7063 : 7054, new Position(12, 14));
+                break;
+        }
+    }
+    public override void OnItemDropped(WorldClient client, Item itemDropped, Position locationDropped) { }
+    public override void OnGossip(WorldClient client, string message) { }
+}
