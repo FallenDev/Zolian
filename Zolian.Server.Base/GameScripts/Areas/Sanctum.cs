f5ad0f9fa69a41d397362b8e706df8cfb9b47362
FallenDev <paul.buck@TheBuckNetwork.com>
2023-12-15 18:59:02 -0500

    Fix: Sanctum

---
+++ Zolian.Server.Base/GameScripts/Areas/Sanctum.cs
@@ -0,0 +1,31 @@
+﻿using Darkages.Network.Client;
+using Darkages.ScriptingBase;
+using Darkages.Sprites;
+using Darkages.Types;
+
+using System.Collections.Concurrent;
+using System.Numerics;
+
+namespace Darkages.GameScripts.Areas;
+
+[Script("Sanctum")]
+public class Sanctum : AreaScript
+{
+    private readonly ConcurrentDictionary<long, Aisling> _playersOnMap = new();
+    public Sanctum(Area area) : base(area) => Area = area;
+    public override void Update(TimeSpan elapsedTime) { }
+    public override void OnMapEnter(WorldClient client) => _playersOnMap.TryAdd(client.Aisling.Serial, client.Aisling);
+    public override void OnMapExit(WorldClient client) => _playersOnMap.TryRemove(client.Aisling.Serial, out _);
+
+    public override void OnPlayerWalk(WorldClient client, Position oldLocation, Position newLocation)
+    {
+        var vectorMap = new Vector2(newLocation.X, newLocation.Y);
+        if (client.Aisling.Pos != vectorMap) return;
+        _playersOnMap.TryAdd(client.Aisling.Serial, client.Aisling);
+
+        if (vectorMap.X is 6 or 4 || vectorMap.Y is 6 or 4)
+            client.SendWorldMap();
+    }
+    public override void OnItemDropped(WorldClient client, Item itemDropped, Position locationDropped) { }
+    public override void OnGossip(WorldClient client, string message) { }
+}
