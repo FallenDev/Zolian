68b68c5811274330634a2a0e29fa523d8697f25c
FallenDev <paul.buck@TheBuckNetwork.com>
2025-07-25 16:03:06 -0400

    Damage Scrolling

--- Zolian.Server.Base/Sprites/Damageable.cs
+++ Zolian.Server.Base/Sprites/Damageable.cs
@@ -1488,7 +1488,44 @@ public class Damageable : Movable
 
         var dmgShow = aisling.DamageCounter.ToString();
         aisling.Client.SendPublicMessage(aisling.Serial, PublicMessageType.Chant, $"{dmgShow}");
+        ShowDmgTierAnimation(aisling);
         aisling.DamageCounter = 0;
+    }
+
+    private static void ShowDmgTierAnimation(Aisling aisling)
+    {
+        switch (aisling.DamageCounter)
+        {
+            case >= 1000000 and < 5000000: // 1M
+                aisling.SendAnimationNearby(405, aisling.Position);
+                break;
+            case >= 5000000 and < 10000000: // 5M
+                aisling.SendAnimationNearby(406, aisling.Position);
+                break;
+            case >= 10000000 and < 20000000: // 10M
+                aisling.SendAnimationNearby(407, aisling.Position);
+                break;
+            case >= 20000000 and < 50000000: // 20M
+                aisling.SendAnimationNearby(408, aisling.Position);
+                break;
+            case >= 50000000 and < 100000000: // 50M
+                aisling.SendAnimationNearby(409, aisling.Position);
+                break;
+            case >= 100000000 and < 500000000: // 100M
+                aisling.SendAnimationNearby(410, aisling.Position);
+                break;
+            case >= 500000000 and < 1000000000: // 500M
+                aisling.SendAnimationNearby(411, aisling.Position);
+                break;
+            case >= 1000000000 and < 2000000000: // 1B
+                aisling.SendAnimationNearby(412, aisling.Position);
+                break;
+            case >= 2000000000: // 2B
+                aisling.SendAnimationNearby(413, aisling.Position);
+                break;
+            default:
+                return;
+        }
     }
 
     #endregion
